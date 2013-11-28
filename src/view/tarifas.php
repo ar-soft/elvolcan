09bacba883d50d4fa516908a5d7ffd15348722b8
ralcaraz <ralcaraz@mac-ralcaraz.local>
2013-11-28 19:28:07 -0300

    Tarifas al 100%

--- src/view/tarifas.php
+++ src/view/tarifas.php
@@ -7,7 +7,59 @@ echo basename($_SERVER['REQUEST_URI']).PHP_EOL;
 	<div id="background"></div>
 	<div class="bullets" id="first"></div>
 	<div class="bullets" id="second"></div>
-	<div id="rectangle"></div>
+	<div id="rectangle">
+		<div id="orange-rectangle">
+			<table>
+				<tbody>
+					<tr>
+						<th>HABITACIONES</th>
+						<th>CONFIGURACIÓN</th>
+						<th>TARIFAS</th>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					<tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					<tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					<tr>
+					<tr>
+						<td>Doble</td>
+						<td>(estándar)</td>
+						<td>$ 400.-</td>
+					</tr>
+					<tr>
+						<td>Triple</td>
+						<td>(estándar)</td>
+						<td>$ 400.-</td>
+					</tr>
+					<tr>
+						<td>Cuádruple</td>
+						<td>(estándar)</td>
+						<td>$ 400.-</td>
+					</tr>
+					<tr>
+						<td>Doble</td>
+						<td>(casco)</td>
+						<td>$ 400.-</td>
+					</tr>
+					<tr>
+						<td>Cuádruple</td>
+						<td>(casco)</td>
+						<td>$ 400.-</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </section>
 <script type="text/javascript">
 
