0a8c9463a42a123ce8ad6adda3d16bf8cfe8b31b
ralcaraz <ralcaraz@mac-ralcaraz.local>
2013-11-28 18:45:32 -0300

    Tarifas

--- src/view/tarifas.php
+++ src/view/tarifas.php
@@ -1,5 +1,13 @@
+<!--<?php
+echo $_SERVER['REQUEST_URI'];
+echo basename($_SERVER['REQUEST_URI']).PHP_EOL;
+?>-->
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 <section id="pricing">
+	<div id="background"></div>
+	<div class="bullets" id="first"></div>
+	<div class="bullets" id="second"></div>
+	<div id="rectangle"></div>
 </section>
 <script type="text/javascript">
 
