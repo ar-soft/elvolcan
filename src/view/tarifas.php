060d9432a06d46c5c1f4cfa2943f430718ee25b9
yramirez <yesica.ramirez@mercadolibre.com>
2013-12-06 23:30:10 -0300

    Centrado luz y banco

--- src/view/tarifas.php
+++ src/view/tarifas.php
@@ -2,46 +2,46 @@
 <section class="content-body">
 	<div class="bullets first"></div>
 	<div class="bullets second"></div>
-	<center><img class="light" src="web-app/img/luz.png"></center>
-	<div id="rectangle">
-		<div id="orange-rectangle">
-			<center>
-			<table>
-				<tbody>
-					<tr class="first">
-						<th>CONFIGURACIÓN</th>
-						<th>HABITACIONES</th>
-						<th>TARIFAS</th>
-					</tr>
-					<tr>
-						<td>Doble</td>
-						<td>(estándar)</td>
-						<td>$ 400.-</td>
-					</tr>
-					<tr>
-						<td>Triple</td>
-						<td>(estándar)</td>
-						<td>$ 400.-</td>
-					</tr>
-					<tr>
-						<td>Cuádruple</td>
-						<td>(estándar)</td>
-						<td>$ 400.-</td>
-					</tr>
-					<tr>
-						<td>Doble</td>
-						<td>(casco)</td>
-						<td>$ 400.-</td>
-					</tr>
-					<tr>
-						<td>Cuádruple</td>
-						<td>(casco)</td>
-						<td>$ 400.-</td>
-					</tr>
-				</tbody>
-			</table>
-			</center>
-			<div><img src="web-app/img/banco.png" id="bank"></div>
+	<center>
+		<img class="light" src="web-app/img/luz.png">
+		<div id="rectangle">
+			<div id="orange-rectangle">
+				<table>
+					<tbody>
+						<tr class="first">
+							<th>CONFIGURACIÓN</th>
+							<th>HABITACIONES</th>
+							<th>TARIFAS</th>
+						</tr>
+						<tr>
+							<td>Doble</td>
+							<td>(estándar)</td>
+							<td>$ 400.-</td>
+						</tr>
+						<tr>
+							<td>Triple</td>
+							<td>(estándar)</td>
+							<td>$ 400.-</td>
+						</tr>
+						<tr>
+							<td>Cuádruple</td>
+							<td>(estándar)</td>
+							<td>$ 400.-</td>
+						</tr>
+						<tr>
+							<td>Doble</td>
+							<td>(casco)</td>
+							<td>$ 400.-</td>
+						</tr>
+						<tr>
+							<td>Cuádruple</td>
+							<td>(casco)</td>
+							<td>$ 400.-</td>
+						</tr>
+					</tbody>
+				</table>
+				<img src="web-app/img/banco.png" class="bank">
+			</div>
 		</div>
-	</div>
+	</center>
 </section>
